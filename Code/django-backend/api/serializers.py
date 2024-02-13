4b106ee3f386ef52b5fa2af8f6b37d04e2923e8a
Shreyash <shreyashrahinj1@gmail.com>
2024-02-13 15:18:02 +0530

    basic api views created

--- Code/django-backend/api/serializers.py
+++ Code/django-backend/api/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework.serializers import ModelSerializer
-from resources.models import LectureSlot
+from resources.models import LectureSlot,Room,Teacher,Course,Division
 
 class LectureSlotSerializer(ModelSerializer):
     class Meta:
         model = LectureSlot
         fields = '__all__'
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+class TeacherSerializer(ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+        
+class CourseSerializer(ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class DivisionSerializer(ModelSerializer):
+    class Meta:
+        model = Division
+        fields = '__all__'
+
--- Code/django-backend/api/views.py
+++ Code/django-backend/api/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from resources.models import LectureSlot,Room,Teacher,Course,Division
+from .serializers import LectureSlotSerializer,RoomSerializer,TeacherSerializer,CourseSerializer,DivisionSerializer
 
 # Create your views here.
-class CreateLectureSlotView():
-    pass
+class CreateListLectureSlotView(generics.ListCreateAPIView):
+    queryset = LectureSlot.objects.all()
+    serializer_class = LectureSlotSerializer
+
+class CreateListRoomView(generics.ListCreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+class CreateListTeacherView(generics.ListCreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class CreateListCourseView(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class CreateListDivisionView(generics.ListCreateAPIView):
+    queryset = Division.objects.all()
+    serializer_class = DivisionSerializer
+
+create_list_lecture_slot_view = CreateListLectureSlotView.as_view()
+create_list_room_view = CreateListRoomView.as_view()
+create_list_teacher_view = CreateListTeacherView.as_view()
+create_list_course_view = CreateListCourseView.as_view()
+create_list_division_view = CreateListDivisionView.as_view()
