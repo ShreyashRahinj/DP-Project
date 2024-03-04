2e2031e5a45ad1b3a55a85911cdc91f102295296
Shreyash <shreyashrahinj1@gmail.com>
2024-03-05 05:10:22 +0530

    changes done

--- Code/django-backend/api/urls.py
+++ Code/django-backend/api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('room/', create_list_room_view),
     path('teacher/', create_list_teacher_view),
     path('course/', create_list_course_view),
-    path('division/', create_list_division_view),
+    path('division/', create_list_division_view),   
 ]
