368558ca2e4fa242c3b93a9d328256f0e782245f
Shreyash <shreyashrahinj1@gmail.com>
2024-04-19 16:54:15 +0530

    input all done

--- Code/frontend/lib/view/components/forms/division_form.dart
+++ Code/frontend/lib/view/components/forms/division_form.dart
@@ -1,8 +1,12 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:frontend/view/constants/colors.dart';
 import 'package:provider/provider.dart';
 
 import '../../../controller/providers/resources_data_provider.dart';
+import '../../../model/course.dart';
+import '../../../model/teacher.dart';
 
 const List<DropdownMenuEntry> teachers = [
   DropdownMenuEntry(
@@ -57,32 +61,27 @@ class DivisionForm extends StatefulWidget {
 }
 
 class _DivisionFormState extends State<DivisionForm> {
-  late final TextEditingController idController;
   late final TextEditingController nameController;
-  late final TextEditingController lectureController;
-  late final TextEditingController capacityController;
+  late Teacher? selectedTeacher;
+  late Course? selectedCourse;
 
   @override
   void initState() {
     super.initState();
-    idController = TextEditingController();
+    selectedCourse = null;
+    selectedTeacher = null;
     nameController = TextEditingController();
-    lectureController = TextEditingController();
-    capacityController = TextEditingController();
   }
 
   void save() {
     final provider = Provider.of<ResourcesDataProvider>(context, listen: false);
-    provider.addCourse(
-      id: idController.text,
+    provider.addDivision(
       name: nameController.text,
-      lectureNo: int.parse(lectureController.text),
-      capacity: int.parse(capacityController.text),
+      course: selectedCourse!,
+      teacher: selectedTeacher!,
     );
-    idController.clear();
+    inspect(provider.divisions);
     nameController.clear();
-    lectureController.clear();
-    capacityController.clear();
   }
 
   void onClickSave() {
@@ -98,111 +97,151 @@ class _DivisionFormState extends State<DivisionForm> {
     Navigator.of(context).pop();
   }
 
+  List<DropdownMenuEntry> getTeachers(List<Teacher> teachers) {
+    List<DropdownMenuEntry> teacherEntries = [];
+    for (Teacher teacher in teachers) {
+      teacherEntries.add(
+        DropdownMenuEntry(
+          label: teacher.name,
+          value: teacher,
+        ),
+      );
+    }
+    return teacherEntries;
+  }
+
+  List<DropdownMenuEntry> getCourses(List<Course> courses) {
+    List<DropdownMenuEntry> courseEntries = [];
+    for (Course course in courses) {
+      courseEntries.add(
+        DropdownMenuEntry(
+          label: course.name,
+          value: course,
+        ),
+      );
+    }
+    return courseEntries;
+  }
+
   @override
   void dispose() {
-    idController.dispose();
+    selectedTeacher = null;
+    selectedCourse = null;
     nameController.dispose();
-    lectureController.dispose();
-    capacityController.dispose();
+
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      backgroundColor: PrimaryTheme.appWhite,
-      title: const Text("Division"),
-      shape: const LinearBorder(),
-      content: Form(
-        child: Container(
-          padding: const EdgeInsets.all(24),
-          child: const Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Text("Name"),
-                  ),
-                  SizedBox(width: 8),
-                  Expanded(
-                    flex: 2,
-                    child: TextField(),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Text("Teacher"),
-                  ),
-                  SizedBox(width: 8),
-                  Expanded(
-                    flex: 2,
-                    child: DropdownMenu(
-                      dropdownMenuEntries: teachers,
+    return Consumer<ResourcesDataProvider>(
+      builder: (context, value, child) => AlertDialog(
+        backgroundColor: PrimaryTheme.appWhite,
+        title: const Text("Division"),
+        shape: const LinearBorder(),
+        content: Form(
+          child: Container(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    const Expanded(
+                      flex: 1,
+                      child: Text("Name"),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Text("Course"),
-                  ),
-                  SizedBox(width: 8),
-                  Expanded(
-                    flex: 2,
-                    child: DropdownMenu(
-                      dropdownMenuEntries: courses,
+                    const SizedBox(width: 8),
+                    Expanded(
+                      flex: 2,
+                      child: TextField(
+                        controller: nameController,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    const Expanded(
+                      flex: 1,
+                      child: Text("Teacher"),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      flex: 2,
+                      child: DropdownMenu(
+                        onSelected: (value) {
+                          setState(() {
+                            selectedTeacher = value;
+                          });
+                        },
+                        dropdownMenuEntries: getTeachers(value.teachers),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    const Expanded(
+                      flex: 1,
+                      child: Text("Course"),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      flex: 2,
+                      child: DropdownMenu(
+                        onSelected: (value) {
+                          setState(() {
+                            selectedCourse = value;
+                          });
+                        },
+                        dropdownMenuEntries: getCourses(value.courses),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
+        actions: [
+          InkWell(
+            onTap: onClickSave,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: PrimaryTheme.tabEnable,
+              ),
+              child: const Text('Save'),
+            ),
+          ),
+          InkWell(
+            onTap: onClickAdd,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: PrimaryTheme.tabDisable,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Text('Save and Add'),
+            ),
+          ),
+          InkWell(
+            onTap: onClickCancel,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: PrimaryTheme.errorText,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Text('Cancel'),
+            ),
+          ),
+        ],
       ),
-      actions: [
-        InkWell(
-          child: Container(
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: PrimaryTheme.tabEnable,
-            ),
-            child: const Text('Save'),
-          ),
-        ),
-        InkWell(
-          child: Container(
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: PrimaryTheme.tabDisable,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: const Text('Save and Add'),
-          ),
-        ),
-        InkWell(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Container(
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: PrimaryTheme.errorText,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: const Text('Cancel'),
-          ),
-        ),
-      ],
     );
   }
 }
